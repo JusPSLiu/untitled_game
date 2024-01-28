6c454ca00027218af180d6575e65ea28d94f74e3
JusPSLiu <juspsliu@yahoo.com>
2024-01-27 22:33:22 -0500

    first commit

---
+++ Scripts/TitleScreen.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@export
+var transition : AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func play():
+	transition.play("fadeour")
+	await transition.animation_finished
+	get_tree().change_scene_to_file("res://Scenes/game.tscn")
